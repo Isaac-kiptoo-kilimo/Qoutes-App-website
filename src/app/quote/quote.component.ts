61d5db00add8b62fe6fb20ace0983785da248507
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-09 17:45:45 +0300

    feature:This will add the constructor

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../model/quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.scss']
 })
 export class QuoteComponent implements OnInit {
+  qoutes: Quote[]=[];
 
   constructor() { }
 
