f84ee780bf629bc1d8d586fbc31ccd5c1b516f69
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-09 23:11:41 +0300

    bug:fixing the path of qoute detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,23 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddQuoteComponent } from './quote/add-quote/add-quote.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QouteDetailComponent } from './quote/qoute-detail/qoute-detail.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:QuoteComponent,
-    // children[
-    //   {
-    //     path:'qoutes/:id',
-    //     component: QuoteDetailComponent
-    //   }
-    // ]
+    path: '',
+    component: QuoteComponent,
+    children: [
+      {
+        path: 'quotes/:id',
+        component: QouteDetailComponent
+      }
+    ]
   },
   {
     path: 'add-quote',
     component: AddQuoteComponent,
   }
-  
   
 ];
 
