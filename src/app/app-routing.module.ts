b7826368f5b9f631326cadb4c1482442299a913f
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-09 18:04:01 +0300

    added app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddQuoteComponent } from './quote/add-quote/add-quote.component';
+import { QuoteComponent } from './quote/quote.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    component:QuoteComponent,
+    // children[
+    //   {
+    //     path:'qoutes/:id',
+    //     component: QuoteDetailComponent
+    //   }
+    // ]
+  },
+  {
+    path: 'add-quote',
+    component: AddQuoteComponent,
+  }
+  
   
 ];
 
