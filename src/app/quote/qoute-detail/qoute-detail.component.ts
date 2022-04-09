b4a0b51cbae05970b28dd6247446b0dd59cb27ef
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-09 18:43:03 +0300

    feature:This will add the selected qoute

--- src/app/quote/qoute-detail/qoute-detail.component.ts
+++ src/app/quote/qoute-detail/qoute-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from 'src/app/model/quote';
 
 @Component({
   selector: 'app-qoute-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qoute-detail.component.scss']
 })
 export class QouteDetailComponent implements OnInit {
+  qoute: Quote | undefined;
 
   constructor() { }
 
