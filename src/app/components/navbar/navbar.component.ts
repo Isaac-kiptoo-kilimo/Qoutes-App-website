dc6b70765bc48157d2f8d244365929593eb6bdf9
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-10 20:17:05 +0300

    fix:This will fix a bug in the date pipe

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,20 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  transform(value: any): number {
-    let today:Date = new Date(); 
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value - todayWithNoTime) 
-    const secondsInDay = 86400;
-    var dateDifferenceSeconds = dateDifference*0.001;
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
-
-    if (dateCounter >= 1){
-      return dateCounter;
-    }else{
-      return 0;
-    }
-  }
+  
 
   constructor() { }
 
